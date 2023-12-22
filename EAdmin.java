d8bc629de8cb9be0ecd96467ca00377e4606895d
QUIET-DEVELOPER <149947159+QUIET-DEVELOPER@users.noreply.github.com>
2023-12-22 21:33:09 +0530

    Update EAdmin.java

--- EAdmin.java
+++ EAdmin.java
@@ -1,21 +1,23 @@
 import java.util.HashSet;
 import java.util.Scanner;
 
+// EAdmin class for managing departments and courses
 public class EAdmin {
-    HashSet<Admin> dept=new HashSet<Admin>();
-	HashSet<Admin> course = new HashSet<Admin>();
-	
+    HashSet<Admin> dept=new HashSet<Admin>(); //HashSet to store departments
+    HashSet<Admin> course = new HashSet<Admin>(); // HashSet to store courses
+
+	// Pre-defined courses and departments
 	Admin course1 = new Admin(2914,"DBMS");
 	Admin course2 = new Admin(1234, "Python");
-
 	
 	Admin dep1=new Admin(05, "CSE");
 	Admin dep2=new Admin(04, "ECE");
 
-	Scanner sc=new Scanner(System.in);
+	Scanner sc=new Scanner(System.in); // Scanner object for user input
 	int id;
 	String name;
-	
+
+	//Constructor to initialize departments and courses
 	public EAdmin() {
 		
 		dept.add(dep1);
@@ -24,20 +26,21 @@ public class EAdmin {
 		course.add(course2);
 	}
 
-	// view all courses
+	// Method to view all courses
 	public void viewAllCourse() {
 		for(Admin crse:course) {
 			System.out.println(crse);
 		}
 	}
-	//view all departments
+	// Method to view all departments
 	public void viewAlldept() {
 		for(Admin dep:dept) {
 			System.out.println(dep);
 		}
 	}
-	//delete department
+	// Method to delete a department
 	public void deletedep() {
+		// Implementation to delete a department based on ID
 		System.out.println("Enter id");
 		id=sc.nextInt();
 		boolean found=false;
@@ -57,8 +60,9 @@ public class EAdmin {
 		}
 	}
 
-	//delete courses
+	// Method to delete a course
 	public void deleteCourse() {
+		// Implementation to delete a course based on ID
 		System.out.println("Enter id");
 		id=sc.nextInt();
 		boolean found=false;
@@ -78,8 +82,9 @@ public class EAdmin {
 		}
 	}
 
-	//add departments
+	// Method to add a department
 	public void adddept() {
+		// Implementation to add a department with ID and name
 		System.out.println("Enter department id:");
 		id=sc.nextInt();
 		System.out.println("Enter department name");
@@ -93,8 +98,9 @@ public class EAdmin {
 		
 	}
 
-	//add courses
+	// Method to add a course
 	public void addCourse() {
+		// Implementation to add a course with ID and name
 		System.out.println("Enter Course id:");
 		id=sc.nextInt();
 		System.out.println("Enter Course name");
